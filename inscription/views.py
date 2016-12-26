72197387b262fa86408aa8531f46765749cfc4a2
Your Name <you@example.com>
2016-12-26 16:47:08 +0100

    first commit

---
+++ inscription/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("welcome to CLL inscrivez-vous içi")
+
+# Create your views here.
